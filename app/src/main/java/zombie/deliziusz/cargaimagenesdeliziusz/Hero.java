0d9c3f084f6c8cf835ec846a11542d4c3d370860
Deliziusz <deliziusz.666@gmail.com>
2021-02-28 17:21:43 -0600

    Iniciamos... en 3.. 2... 1... Último día del mes.... Domingo... Trabajando.... Blablabla
    Proyecto que carga imágenes desde Internet

---
+++ app/src/main/java/zombie/deliziusz/cargaimagenesdeliziusz/Hero.java
@@ -0,0 +1,18 @@
+package zombie.deliziusz.cargaimagenesdeliziusz;
+
+public class Hero {
+
+    private String name;
+    private String image;
+
+    public Hero (String name, String image){
+        this.name = name;
+        this.image = image;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getImage(){
+        return image;
+    }
+}
---
+++ app/src/main/java/zombie/deliziusz/cargaimagenesdeliziusz/HeroAdapter.java
@@ -0,0 +1,4 @@
+package zombie.deliziusz.cargaimagenesdeliziusz;
+
+public class HeroAdapter {
+}
