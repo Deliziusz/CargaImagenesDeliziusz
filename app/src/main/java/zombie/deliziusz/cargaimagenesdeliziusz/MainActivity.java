c6cb46271cb08a167ccb52866d52755ffbbb8e7e
Deliziusz <deliziusz.666@gmail.com>
2021-02-28 19:20:22 -0600

    ¿Acabamos? ¿Por fin? claro... al parecer! Después de unos problemitas no tan técnicos, lo logré Programación Móvil II. Karla Yazmín García Pérez.

--- app/src/main/java/zombie/deliziusz/cargaimagenesdeliziusz/MainActivity.java
+++ app/src/main/java/zombie/deliziusz/cargaimagenesdeliziusz/MainActivity.java
@@ -1,14 +1,77 @@
 package zombie.deliziusz.cargaimagenesdeliziusz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "https://simplifiedcoding.net/demos/view-flipper/heroes.php";
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<Hero> heroes;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progress);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        heroes = new ArrayList<>();
+        load();
+    }
+    public void load(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                (response) -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        JSONArray jsonArray = jsonObject.getJSONArray("heroes");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            Hero hero = new Hero(
+                                    object.getString("name"),
+                                    object.getString("imageurl")
+                            );
+                            heroes.add(hero);
+                        }
+                        adapter = new HeroAdapter(heroes, getApplicationContext());
+                        progressBar.setVisibility(View.GONE);
+                        recyclerView.setAdapter(adapter);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                (error) -> {
+                    Toast.makeText(MainActivity.this, "ERROR: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
